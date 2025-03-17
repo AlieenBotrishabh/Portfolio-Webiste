5de969a51b24720939378fa97629c33e541fa745
Rishabh Kumar <119288381+AlieenBotrishabh@users.noreply.github.com>
2025-03-17 19:43:01 +0530

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
-import { HashRouter } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css"; // Tailwind CSS (optional)
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
+  <React.StrictMode>
     <App />
-  </HashRouter>
+  </React.StrictMode>
 );
