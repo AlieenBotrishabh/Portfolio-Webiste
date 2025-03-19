090cc9ea68e0f36251cfa2435b325a88a463c5ca
Rishabh Kumar <119288381+AlieenBotrishabh@users.noreply.github.com>
2025-03-19 15:21:27 +0530

    routes

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -23,7 +23,7 @@ export default function Portfolio() {
     e.preventDefault();
   
     try {
-      const response = await axios.post("http://localhost:5000/form", formData);
+      const response = await axios.post("portfolio-webiste-68.vercel.app/form", formData);
       setMessage(response.data.msg);
   
       // Clear form data
