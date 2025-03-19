e5ea59953687b3baa1caa89b7716e6ade50ed687
Rishabh Kumar <119288381+AlieenBotrishabh@users.noreply.github.com>
2025-03-19 20:02:24 +0530

    style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],  // Ensure this includes the right paths
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
